aa6d0c41c2555be9327549c0e494489fbf885e49
abdurashidhufjkl <abdurashidrashimov@gmail.com>
2025-12-29 13:11:10 +0500

    fakeshop

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,34 @@
 const productsContainer = document.getElementById("products");
 
-axios
-  .get("https://fakestoreapi.com/products")
-  .then(function (response) {
-    const products = response.data;
+function fetchProducts() {
+  const url = "https://fakestoreapi.com/products";
 
-    productsContainer.innerHTML = "";
-
-
-    products.slice(0, 4).forEach(function (product) {
-      const card = document.createElement("div");
-      card.className = "product";
-
-      card.innerHTML = `
-                <img src="${product.image}" alt="${product.title}">
-                <h3>${product.title.substring(0, 30)}...</h3>
-                <p>${product.price} $</p>
-                <button>Savatga qo‘shish</button>
-            `;
-
-      productsContainer.appendChild(card);
+  axios
+    .get(url)
+    .then((response) => {
+      showData(response.data);
+    })
+    .catch((error) => {
+      console.error("Xatolik:", error);
+      productsContainer.innerHTML = "<p>Mahsulotlar yuklanmadi</p>";
     });
-  })
-  .catch(function (error) {
-    console.error("Xatolik:", error);
-    productsContainer.innerHTML = "<p>Mahsulotlar yuklanmadi</p>";
-  });
+}
 
+function showData(products) {
+  productsContainer.innerHTML = "";
+  const firstFour = products.slice(0, 4);
+  firstFour.forEach((product) => {
+    const card = document.createElement("div");
+    card.className = "product";
+    card.innerHTML = `
+            <img src="${product.image}" alt="${product.title}">
+            <h3>${product.title.substring(0, 30)}...</h3>
+            <p>${product.price} $</p>
+            <button>Savatga qo‘shish</button>
+        `;
+
+    productsContainer.appendChild(card);
+  });
+}
+
+window.addEventListener("DOMContentLoaded", fetchProducts);
--- scripts/product.js
+++ scripts/product.js
@@ -1,27 +1,29 @@
 const productsContainer = document.getElementById("products");
-
-axios
-  .get("https://fakestoreapi.com/products")
-  .then(function (response) {
-    const products = response.data;
-
-    productsContainer.innerHTML = "";
-
-    products.forEach(function (product) {
-      const card = document.createElement("div");
-      card.className = "product";
-
-      card.innerHTML = `
-                <img src="${product.image}" alt="${product.title}">
-                <h3>${product.title.slice(0 ,30)}...</h3>
-                <p>${product.price} $</p>
-                <button>Savatga qo‘shish</button>
-            `;
-
-      productsContainer.appendChild(card);
+function fetchAllProducts() {
+  const url = "https://fakestoreapi.com/products";
+  axios
+    .get(url)
+    .then((response) => {
+      showAllData(response.data);
+    })
+    .catch((error) => {
+      console.error("Xatolik:", error);
+      productsContainer.innerHTML = "<p>Mahsulotlar yuklanmadi</p>";
     });
-  })
-  .catch(function (error) {
-    console.error(error);
-    productsContainer.innerHTML = "<p>Xatolik yuz berdi</p>";
+}
+function showAllData(products) {
+  productsContainer.innerHTML = "";
+  products.forEach((product) => {
+    const card = document.createElement("div");
+    card.className = "product";
+
+    card.innerHTML = `
+            <img src="${product.image}" alt="${product.title}">
+            <h3>${product.title.slice(0 , 30)}...</h3>
+            <p>${product.price} $</p>
+            <button>Savatga qo‘shish</button>
+        `;
+    productsContainer.appendChild(card);
   });
+}
+window.addEventListener("DOMContentLoaded", fetchAllProducts);
